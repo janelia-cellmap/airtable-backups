aa898abba70f618fe646b1c4f1a754eb240a49c4
Davis Vann Bennett <davis.v.bennett@gmail.com>
2023-07-25 21:56:58 -0400

    fix: remove hard-coded home directory from cron.sh

--- src/cron.sh
+++ src/cron.sh
@@ -4,7 +4,7 @@
 source $HOME/.zshrc;
 set -a # automatically export all variables
 # get environment variables from .env
-source /home/bennettd@hhmi.org/dev/airtable-backups/.env
+source $HOME/dev/airtable-backups/.env
 set +a
 # run the backup
 airtable-backup
